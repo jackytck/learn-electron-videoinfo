27e45d853b4db697cc0df8d5cf94384325ebbff2
Jacky Tang <jackytck@gmail.com>
2017-06-09 00:05:46 +0800

    :hammer: send file path from renderer to main via ipc

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const electron = require('electron')
 
 const {
   app,
-  BrowserWindow
+  BrowserWindow,
+  ipcMain
 } = electron
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({})
   mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
+
+ipcMain.on('video:submit', (event, path) => {
+  console.log(path)
 })
